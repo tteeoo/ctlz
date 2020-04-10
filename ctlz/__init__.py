a9525fe02f459d5c2d0fe471fda021619642aa60
Theo Henson <theodorehenson@protonmail.com>
2020-04-09 22:38:32 -0400

    Added serialization

--- ctlz/__init__.py
+++ ctlz/__init__.py
@@ -8,20 +8,30 @@ import configparser
 import ctlz.text
 import ctlz.exceptions
 
+"""
+TODO:
+    - Add more config types
+"""
+
 ## Class for easy management of configuration files
 #
 #  Currently only deserializes from a list of paths
 class Config:
+
+    def __validate_format(self, fmt):
+        if fmt not in ["json", "ini"]:
+            raise exceptions.InvalidConfigFormat("Invalid config file format, valid formats include 'json' and 'ini'")
+        else: self.fmt = fmt
 
     ## The constructor method
     #
     #  Initializes properties from parameters
     def __init__(self, paths, fmt, default=None):
         self.paths = paths
-        self.fmt = fmt
+        self.location = None
         self.default = default
         self.data = default
-        self.read_location = None
+        self.__validate_format(fmt)
 
     ## Reads config file at the first valid path in self.paths and return it deserialized
     #
@@ -33,23 +43,23 @@ class Config:
                 good_paths.append(path)
         if len(good_paths) < 1:
             if self.default != None:
-                return self.default
-            else:
                 raise exceptions.NoConfigFileFound("No file found or default provided")
+            else:
+                return self.default
         else:
-            self.read_location = good_paths[0]
+            self.location = good_paths[0]
 
         # json
         if self.fmt == "json":
             if self.default != None:
                 try:
-                    with open(self.read_location) as f:
+                    with open(self.location) as f:
                         self.data = json.load(f.read())
                     return self.data
                 except:
                     return self.default
             else:
-                with open(self.read_location) as f:
+                with open(self.location) as f:
                     self.data = json.load(f.read())
                 return self.data
 
@@ -58,19 +68,53 @@ class Config:
             if self.default != None:
                 try:
                     self.data = configparser.ConfigParser()
-                    self.data.read(self.read_location)
+                    self.data.read(self.location)
                     return self.data
                 except:
                     return self.default
             else:
                 self.data = configparser.ConfigParser()
-                self.data.read(self.read_location)
+                self.data.read(self.location)
                 return self.data
 
+    ## Writes config to the first valid path in self.paths
+    #
+    #  Currently only supports json and ini-style config files
+    def serialize_safe(self):
+        good_paths = []
+        for path in self.paths:
+            if os.path.isfile(path):
+                good_paths.append(path)
+        if len(good_paths) < 1:
+            raise exceptions.NoConfigFileFound("No file found to write to")
         else:
-            raise exceptions.InvalidConfigFormat("Invalid config file format, valid formats include 'json' and 'ini'")
+            self.location = good_paths[0]
 
-        #TODO: add more config formats
+        # json
+        if self.fmt == "json":
+            with open(self.location, "w") as f:
+                json.dump(self.data, f.write())
+
+        # ini
+        elif self.fmt == "ini":
+            with open(self.location, "w") as f:
+                self.data.write(f)
+
+    ## Writes config to the specified path
+    #
+    #  Currently only supports json and ini-style config files
+    def serialize(self, path):
+
+        # json
+        if self.fmt == "json":
+            with open(path, "w") as f:
+                json.dump(self.data, f.write())
+
+        # ini
+        elif self.fmt == "ini":
+            with open(path, "w") as f:
+                self.data.write(f)
+
 
 class Modes:
     pass
